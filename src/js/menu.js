e772429c133801c5a2b905297b28cde5baae5ced
matheuskya <kenzoyamashitaasano@gmail.com>
2025-06-17 12:34:56 -0300

    Introdução dos menus para Cadastro de transportadora e merge

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Seleciona os itens do submenu de funcionários
   const linksMenu = document.querySelectorAll(
-    "[data-menu], .produtos.submenu ul li a, .funcionarios.submenu ul li a, .caixa.submenu ul li a"
+    "[data-menu], .produtos.submenu ul li a, .funcionarios.submenu ul li a, .caixa.submenu ul li a, .transportadora.submenu ul li a"
   );
 
   // Mapeia o texto do menu para a classe da seção correspondente
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
     "Cadastrar produto": "cadastrar-produto",
     "Listar produtos": "listar-produtos",
     "Atualizar produto": "atualizar-produto",
-    "Registrar venda": "registrar-venda"
+    "Registrar venda": "registrar-venda",
+    "Cadastrar transportadora": "cadastrar-transportadora",
     // Adicione outros mapeamentos conforme criar novas seções
   };
 
